90280ff75508a96c8b50f9e1a7b102da500ab2fc
Andrei <an.kolykhavlo@mail.ru>
2023-08-17 18:04:43 +0300

    Created echo bot for beginning

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(non_stop=True)

36982bbb01de92669c696e6e6bec9947d5862d37
D0ppe1 <140817454+d0ppe1@users.noreply.github.com>
2023-08-29 17:46:40 +0300

    Beginning of realization buttons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 from telebot import types
-import operations
+from operations import*
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
@@ -17,13 +17,24 @@ def greetings(message):
 def start(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     btn1 = types.KeyboardButton(text='Начать отсчёт')
+    if message.text == 'Начать отсчёт':
+        bot.send_message(message.chat.id, f'Отсчет времени начался:  {doppel.start_work()}\nУдачи 😉')
     btn2 = types.KeyboardButton(text='Поставить на паузу')
+    if message.text == 'Поставить на паузу':
+        bot.send_message(message.chat.id, 'в разработке Поставить на паузу ')
     btn3 = types.KeyboardButton(text='Продолжить')
+    if message.text == 'Продолжить':
+        bot.send_message(message.chat.id, 'в разработке Продолжить')
     btn4 = types.KeyboardButton(text='Закончить отсчёт')
+    if message.text == 'Закончить отсчёт':
+        bot.send_message(message.chat.id, f'Отсчёт времени закончиося:  {doppel.end_work()}\nТы сегодня молодец!😇 ')
     btn5 = types.KeyboardButton(text='Посмотреть количество часов')
+    if message.text == 'Посмотреть количество часов':
+        bot.send_message(message.chat.id, 'в разработке Посмотреть количество часов')
+
 
     keyboard.add(btn1, btn2, btn3, btn4, btn5)
-    bot.send_message(message.chat.id, '1', reply_markup=keyboard)
+    # bot.send_message(message.chat.id, '1', reply_markup=keyboard)
 
     # bot.send_message(message.chat.id, message.text)
 

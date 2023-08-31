904364b6108e35b4223b157898218db785c29933
D0ppe1 <140817454+d0ppe1@users.noreply.github.com>
2023-08-31 21:20:16 +0300

    Reworked keyboard,add 2 new func

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
 import telebot
 import os
 from telebot import types
-from operations import*
+from operations import User
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
+data = {}
 
+
+def registr_user(user_id):
+    '''
+    Check is in data.keys() an user_id and if not func create key:val
+    :param user_id: int
+    :return:
+    '''
+    print(data)
+    if user_id not in data.keys():
+        data[user_id] = User(user_id=user_id)
+
+
+def get_keyboard():
+    '''
+    Crate keyboard
+    :return: keyboard
+    '''
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton(text='Начать отсчёт')
+    btn2 = types.KeyboardButton(text='Поставить на паузу')
+    btn3 = types.KeyboardButton(text='Продолжить')
+    btn4 = types.KeyboardButton(text='Закончить отсчёт')
+    btn5 = types.KeyboardButton(text='Посмотреть количество часов')
+    keyboard.add(btn1, btn2, btn3, btn4, btn5)
+    return keyboard
 @bot.message_handler(commands=['start'])
 def greetings(message):
-    bot.send_message(message.chat.id, "Добро пожаловать {0.first_name}!\nЯ - <b>{1.first_name}</b>,"
-                                      " бот созданный для отслеживания твоего прогресса."
-                     .format(message.from_user, bot.get_me()), parse_mode='html')
+    registr_user(user_id=message.from_user.id)
+    greeting_text = f"Добро пожаловать {message.from_user.first_name}!\nЯ - <b>{bot.get_me().first_name}</b>," \
+                    " бот созданный для отслеживания твоего прогресса."
+    bot.send_message(chat_id=message.chat.id, text=greeting_text, parse_mode='html',reply_markup=get_keyboard())
 
 
 @bot.message_handler(content_types=['text'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn1 = types.KeyboardButton(text='Начать отсчёт')
+    registr_user(user_id=message.from_user.id)
+    text = None
     if message.text == 'Начать отсчёт':
-        bot.send_message(message.chat.id, f'Отсчет времени начался:  {doppel.start_work()}\nУдачи 😉')
-    btn2 = types.KeyboardButton(text='Поставить на паузу')
-    if message.text == 'Поставить на паузу':
-        bot.send_message(message.chat.id, 'в разработке Поставить на паузу ')
-    btn3 = types.KeyboardButton(text='Продолжить')
-    if message.text == 'Продолжить':
-        bot.send_message(message.chat.id, 'в разработке Продолжить')
-    btn4 = types.KeyboardButton(text='Закончить отсчёт')
-    if message.text == 'Закончить отсчёт':
-        bot.send_message(message.chat.id, f'Отсчёт времени закончиося:  {doppel.end_work()}\nТы сегодня молодец!😇 ')
-    btn5 = types.KeyboardButton(text='Посмотреть количество часов')
-    if message.text == 'Посмотреть количество часов':
-        bot.send_message(message.chat.id, 'в разработке Посмотреть количество часов')
-
-
-    keyboard.add(btn1, btn2, btn3, btn4, btn5)
-    # bot.send_message(message.chat.id, '1', reply_markup=keyboard)
+        text = '///'
+    elif message.text == 'Поставить на паузу':
+        text = 'в разработке Поставить на паузу '
+    elif message.text == 'Продолжить':
+        text = 'в разработке Продолжить'
+    elif message.text == 'Закончить отсчёт':
+        text = '///'
+    elif message.text == 'Посмотреть количество часов':
+        text = 'в разработке Посмотреть количество часов'
+    if text:
+        bot.send_message(chat_id=message.chat.id, text=text, parse_mode='html',reply_markup=get_keyboard())
 
     # bot.send_message(message.chat.id, message.text)
 

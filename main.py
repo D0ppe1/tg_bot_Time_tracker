5be056bcf5158b6a4c6cd68e37cdbd9f750da003
D0ppe1 <140817454+d0ppe1@users.noreply.github.com>
2023-09-12 20:57:13 +0300

    added classes: Data, App, Category added func: register_user, get_keyboard, register_handlers, create_category, remove_category

--- main.py
+++ main.py
@@ -1,63 +1,75 @@
 import telebot
 import os
-from telebot import types
-from operations import User
-
-bot = telebot.TeleBot(os.getenv('TOKEN'))
-
-data = {}
+# from telebot import types
+from operations import User, Data
 
 
-def registr_user(user_id):
-    '''
-    Check is in data.keys() an user_id and if not func create key:val
-    :param user_id: int
-    :return:
-    '''
-    print(data)
-    if user_id not in data.keys():
-        data[user_id] = User(user_id=user_id)
+class App:
+
+    def __init__(self):
+        self.bot = telebot.TeleBot(os.getenv('TOKEN'))
+        Data.load()
+        self.register_handlers()
+        self.bot.polling(non_stop=True)
+
+    @staticmethod
+    def register_user(user_id):
+        """
+
+        :param user_id: int
+        :return: None
+        """
+        print(Data.users)
+        # Check is in data.keys() an user_id and if not, func is creat key:val
+        if user_id not in Data.users.keys():
+            Data.users[user_id] = User(user_id=user_id)
+            Data.save()
+
+    @staticmethod
+    def get_keyboard():
+        """
+        Crate keyboard
+        :return: keyboard
+        """
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        btn1 = telebot.types.KeyboardButton(text='Начать отсчёт')
+        btn2 = telebot.types.KeyboardButton(text='Поставить на паузу')
+        btn3 = telebot.types.KeyboardButton(text='Продолжить')
+        btn4 = telebot.types.KeyboardButton(text='Закончить отсчёт')
+        btn5 = telebot.types.KeyboardButton(text='Посмотреть количество часов')
+        keyboard.add(btn1, btn2, btn3, btn4, btn5)
+        return keyboard
+
+    def register_handlers(self):
+        @self.bot.message_handler(commands=['start'])
+        def start_handler(message):
+            self.register_user(user_id=message.from_user.id)
+            greeting_text = f"Добро пожаловать {message.from_user.first_name}!" \
+                            f"\nЯ - <b>{self.bot.get_me().first_name}</b>," \
+                            " бот созданный для отслеживания твоего прогресса."
+            self.bot.send_message(chat_id=message.chat.id, text=greeting_text, parse_mode='html',
+                                  reply_markup=self.get_keyboard())
+
+        @self.bot.message_handler(content_types=['text'])
+        def text_handler(message):
+            self.register_user(user_id=message.from_user.id)
+            text = None
+            if message.text == 'Начать отсчёт':
+                text = '///'
+            elif message.text == 'Поставить на паузу':
+                text = 'в разработке Поставить на паузу '
+            elif message.text == 'Продолжить':
+                text = 'в разработке Продолжить'
+            elif message.text == 'Закончить отсчёт':
+                text = '///'
+            elif message.text == 'Посмотреть количество часов':
+                text = 'в разработке Посмотреть количество часов'
+            if text:
+                self.bot.send_message(chat_id=message.chat.id, text=text, parse_mode='html',
+                                      reply_markup=self.get_keyboard())
+
+            # bot.send_message(message.chat.id, message.text)
 
 
-def get_keyboard():
-    '''
-    Crate keyboard
-    :return: keyboard
-    '''
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    btn1 = types.KeyboardButton(text='Начать отсчёт')
-    btn2 = types.KeyboardButton(text='Поставить на паузу')
-    btn3 = types.KeyboardButton(text='Продолжить')
-    btn4 = types.KeyboardButton(text='Закончить отсчёт')
-    btn5 = types.KeyboardButton(text='Посмотреть количество часов')
-    keyboard.add(btn1, btn2, btn3, btn4, btn5)
-    return keyboard
-@bot.message_handler(commands=['start'])
-def greetings(message):
-    registr_user(user_id=message.from_user.id)
-    greeting_text = f"Добро пожаловать {message.from_user.first_name}!\nЯ - <b>{bot.get_me().first_name}</b>," \
-                    " бот созданный для отслеживания твоего прогресса."
-    bot.send_message(chat_id=message.chat.id, text=greeting_text, parse_mode='html',reply_markup=get_keyboard())
-
-
-@bot.message_handler(content_types=['text'])
-def start(message):
-    registr_user(user_id=message.from_user.id)
-    text = None
-    if message.text == 'Начать отсчёт':
-        text = '///'
-    elif message.text == 'Поставить на паузу':
-        text = 'в разработке Поставить на паузу '
-    elif message.text == 'Продолжить':
-        text = 'в разработке Продолжить'
-    elif message.text == 'Закончить отсчёт':
-        text = '///'
-    elif message.text == 'Посмотреть количество часов':
-        text = 'в разработке Посмотреть количество часов'
-    if text:
-        bot.send_message(chat_id=message.chat.id, text=text, parse_mode='html',reply_markup=get_keyboard())
-
-    # bot.send_message(message.chat.id, message.text)
-
-
-bot.polling(non_stop=True)
+if __name__ == '__main__':
+    app = App()
--- operations.py
+++ operations.py
@@ -25,6 +25,7 @@ class User:
         self.user_id = user_id
         self.old_jobs = []
         self.job = None
+        self.categories = {}
 
     def start_work(self):
         self.job = Job()
@@ -35,32 +36,55 @@ class User:
             self.old_jobs.append(self.job)
             self.job = None
 
+    def create_category(self, name):
+        if name not in self.categories.keys():
+            self.categories[name] = (Category(name, self))
 
-def save(data):
-    '''
-    Func so save data with pickle
-    :param data: dict
-    :return:
-    '''
-    try:
-        with open('data.pickle', 'wb') as f:
-            pickle.dump(data, f)
-    except:
-        print('Ошибка при сохранении данных')
+    def remove_category(self, name):
+        if name in self.categories.keys():
+            self.categories.pop(name)
 
 
-def load():
-    '''
-    Func so load data with pickle
-    :return: data{} or empty{}
-    '''
-    try:
-        with open('data.pickle', 'rb') as f:
-            data = pickle.load(f)
-    except:
-        print('Ошибка при загрузке данных')
-    # isinstance проверяет чтобы "data" была "dict",т.е. загружаемый файл был словарем
-    if isinstance(data, dict):
-        return data
-    else:
-        return {}
+class Category:
+    def __init__(self, name: str, author: User):
+        """
+        Creating instance of Category.
+        :param name: Name of Category.
+        :param author: User who creates a Category.
+        """
+        self.name = name
+        self.author = author
+
+
+class Data:
+    users = {}
+
+    @staticmethod
+    def save():
+        """
+        Func so save data with pickle
+        :param data: dict
+        :return:
+        """
+        try:
+            with open('data.pickle', 'wb') as f:
+                pickle.dump(Data.users, f)
+        except:
+            print('Ошибка при сохранении данных')
+
+    @staticmethod
+    def load():
+        """
+        Func so load data with pickle
+        :return: data{} or empty{}
+        """
+        try:
+            with open('data.pickle', 'rb') as f:
+                data = pickle.load(f)
+        except:
+            print('Ошибка при загрузке данных')
+        # isinstance проверяет чтобы "data" была "dict",т.е. загружаемый файл был словарем
+        if isinstance(data, dict):
+            Data.users = data
+        else:
+            Data.users = {}
